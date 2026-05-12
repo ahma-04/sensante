d7e9dc42b52fbd1dcbbf237ffc64697585d32834
Etudiant ESP <etudiant@esp.sn>
2026-05-12 15:32:22 +0000

    Ajout scripts utilitaires bat

---
+++ git_cmd1_status.bat
@@ -0,0 +1,14 @@
+@echo off
+title git status
+cd /d C:\Users\LENOVO\sensante
+color 0A
+echo.
+echo  > git status
+echo.
+git status
+echo.
+echo  ============================================================
+echo   .env n'apparait PAS = gitignore fonctionne correctement
+echo  ============================================================
+echo.
+pause
